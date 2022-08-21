1fed832627c8c8960630b11b49e12de6b4ab932b
RhysMorgan1 <91530448+RhysMorgan1@users.noreply.github.com>
2022-08-21 19:22:01 +0100

    Added more advanced reading function

--- uniplot.py
+++ uniplot.py
@@ -1,3 +1,13 @@
 import gzip
-for l in gzip.open("uniprot_receptor.xml.gz"):
-    print(l.decode().strip())
+from Bio import SeqIO
+
+
+
+#for l in gzip.open("uniprot_receptor.xml.gz"):
+#    print(l.decode().strip())
+
+
+
+handle=gzip.open("uniprot_receptor.xml.gz")
+for record in SeqIO.parse(handle, "uniprot-xml"):
+    print(record)

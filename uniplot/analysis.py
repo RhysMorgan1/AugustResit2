a0cb1debcea354668574a530945e74dbcd596123
RhysMorgan1 <91530448+RhysMorgan1@users.noreply.github.com>
2022-09-02 12:35:30 +0100

    Changed average length of taxa function to put the average of each taxa into a dictionary

--- uniplot/analysis.py
+++ uniplot/analysis.py
@@ -11,14 +11,28 @@ def average_len(records):
     return round(Average)
 
 
-def average_len_taxa(records):
+def average_len_taxa(records, DataDepth):
     """Returns the average length for the top level taxa"""
     record_by_taxa = {}
-    for r in records:
-        taxa = r.annotations["taxonomy"][0]
+    RecordCount = {}
+    RecordNames = {}
+
+    DataDepth = int(DataDepth)
+    if DataDepth > 0:
+        DataDepth = DataDepth -1
+    for record in records:
+        taxa = record.annotations["taxonomy"][DataDepth]
+        if taxa in record_by_taxa:
+            RecordCount[taxa] = RecordCount[taxa] + 1
+            record_by_taxa[taxa][0] = record_by_taxa[taxa][0]+1
+            record_by_taxa[taxa][1] = record_by_taxa[taxa][1] + len(record)
+        else:
+            record_by_taxa.setdefault(taxa, [1, len(record)])
+            RecordCount.setdefault(taxa, 1)
+            RecordNames.append(taxa)
+    for i in RecordNames:
+        record_by_taxa[i][1] = record_by_taxa[i][1]/RecordCount[i]
+    return record_by_taxa
 
 
-        record_by_taxa.setdefault(taxa, []).append(r)
-
-    return {taxa:average_len(record) for (taxa, record) in record_by_taxa.items()}
 
